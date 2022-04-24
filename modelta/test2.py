6524bf4660b84a6dbb3075d77c4409a78ce19cb3
Chenjy0212 <1026224216@qq.com>
2022-04-24 09:58:42 +0800

    main

--- modelta/test2.py
+++ modelta/test2.py
@@ -1,4 +1,4 @@
-#import modelta
+import modelta
 
-#modelta.demola()
-#print(modelta.scoremat('modelta/ExampleFile/tree.nwk','modelta/ExampleFile/Name2Type.csv','modelta/ExampleFile/tree.nwk','modelta/ExampleFile/Name2Type.csv'))
+modelta.demola()
+print(modelta.scoremat('modelta/ExampleFile/tree.nwk','modelta/ExampleFile/Name2Type.csv','modelta/ExampleFile/tree.nwk','modelta/ExampleFile/Name2Type.csv'))
