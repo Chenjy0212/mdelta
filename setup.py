bdff02cea3766746594b7fd6db8951fc05cfb2d5
Chenjy0212 <1026224216@qq.com>
2022-09-19 17:24:00 +0800

    linux_test

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/Chenjy0212/mdelta'
 EMAIL = '1026224216@qq.com'
 AUTHOR = 'EeWhile(Jingyu Chen)'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '1.0.32'
+VERSION = '1.0.34'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
